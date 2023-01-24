16919230c34f5fdf9037427541fe40b70e706f61
Weslley <wediweslley@gmail.com>
2023-01-24 03:40:11 -0300

    Primeiro commit. Desenvolvendo o front-end

---
+++ react-pitagoras/src/index.js
@@ -0,0 +1,68 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import './index.css'
+
+
+
+class App extends React.Component {
+    render() {
+        return (
+            <div className='Janela-Principal'>
+
+
+                <h1 className='Letra1'>Calculadora </h1>
+                <h1 className='Letra2'>de </h1>
+                <h1 className='Letra3'>pitagoras!</h1>
+
+
+
+                <div className='bloco'>
+                    <p className="instrucao">
+                        Insira dois valores para calcular o lado que falta do
+                        triangulo:
+                    </p>
+
+
+
+                    <form class="form" id="form">
+                        <label for="a">
+                            a:
+                            <input name="a" id="a" type="number" />
+                        </label>
+                        <label for="b">
+                            b:
+                            <input  name="a" id="a" type="number" />
+                        </label>
+                        <label for="c">
+                            c:
+                            <input  name="c" id="c" type="number" />
+                        </label>
+                        <button>Calcular</button>
+
+                    </form>
+
+
+                    <div className='imagem'>
+
+                        <img src="imagem/foto.png" alt='Imagem'></img>
+
+                    </div></div>
+
+
+
+                <footer className="footer">
+                    <p>
+                        Feito por{" "}
+                        <a href="https://github.com/wedi0">
+                            Weslley Augusto
+                        </a>
+                    </p>
+                </footer>
+
+            </div>
+        )
+
+    }
+}
+
+ReactDOM.render(<App />, document.getElementById('root'))
