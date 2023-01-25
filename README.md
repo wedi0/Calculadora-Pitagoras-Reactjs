3606b0ab2b0e4e849443f5977e1f8729a9b252f4
wedi0 <wediweslley@gmail.com>
2023-01-25 04:15:41 -0300

    Construção do back-end

--- README.md
+++ README.md
@@ -3,7 +3,7 @@
 **Autor**: Weslley Augusto de Oliveira
 
 **Resumo:** O desafio é desenvolver uma calculadora que será capaz de calcular a relação entre os lados de um triângulo
-retângulo. Tudo isso utilizando javascript, HTML5, CSS3  e uma API em Python utilizando flask ou django.
+retângulo. Tudo isso utilizando **avascript**, **HTML5**, **CSS3**  e uma API em **Python** utilizando **Flask** ou **Django**.
 
 
 
@@ -20,24 +20,30 @@ retângulo. Tudo isso utilizando javascript, HTML5, CSS3  e uma API em Python ut
 Pois bem, neste começo como o desafio pedia front-end usando javascript, optei por aprender e utilizar o framework ReactJS. Então tive que pesquisar como instalar e usar, logo a seguir vou mostar como foi feita essa parte. 
 
 ## React-js
+  
+-----------------
+### O QUE É REACT-JS?
    
-
-   <center> O QUE É REACT-JS? </center>
 
 ````
 O React é uma biblioteca front-end JavaScript de código aberto com foco em criar interfaces de usuário em páginas web.
 ````
 
-<center>COMO BAIXAR, INSTALAR E USAR? </center>
-  
-    
-
+### COMO BAIXAR, INSTALAR E USAR? 
+   
 * 1º passo, é baixar e instalar o [NodeJS](https://nodejs.org/en/download/).
+
 * 2º passo, criar um diretório para seu projeto.
+
 * 3º passo, instalar o React com o comando: `npm create-react-app my-app ` 
+
 * 4º passo, para começar a usar, basta digitar o seguinte comando(dentro do diretório my-app que criou): `npm start`
+
 * 5º passo, colocar a mão na massa e começar a programar.
 
+### Links que me ajudaram:
 
+* Site oficial do [React](https://pt-br.reactjs.org/docs/getting-started.html), porque lá tem a documentação e tutorias muito bons.
+* Esse [video](https://www.youtube.com/watch?v=tPdoxF0kunU) fala sobre a instalação.
 
 
--- src/App.js
+++ src/App.js
@@ -1,67 +1,82 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './index.css'
 
-class App extends React.Component {
-
-    render() {
-        return (
-            <div className='Janela-Principal'>
+function App() {
 
 
-                <h1 className='Letra1'>Calculadora </h1>
-                <h1 className='Letra2'>de </h1>
-                <h1 className='Letra3'>pitagoras!</h1>
+    //--------Back-end------------
 
-
-
-                <div className='bloco'>
-                    <p className="instrucao">
-                        Insira dois valores para calcular o lado que falta do
-                        triangulo:
-                    </p>
+    // Variáveis
+    const [a, seta] = useState(0) // Variável a com uma função de set
+    const [b, setb] = useState(0) // Variável b com uma função de set
+    const [c, setc] = useState(0) // Variável c com uma função de set
 
 
 
-                    <form class="form" id="form">
-                        <label for="a">
-                            a:
-                            <input name="a" id="a" type="number" />
-                        </label>
-                        <label for="b">
-                            b:
-                            <input name="a" id="a" type="number" />
-                        </label>
-                        <label for="c">
-                            c:
-                            <input name="c" id="c" type="number" />
-                        </label>
-                        <button>Calcular</button>
-
-                    </form>
-
-
-                    <div className='imagem'>
-
-                        <img src="imagem/foto.png" alt='Imagem'></img>
-
-                    </div></div>
 
 
 
-                <footer className="footer">
-                    <p>
-                        Feito por{" "}
-                        <a href="https://github.com/wedi0">
-                            Weslley Augusto
-                        </a>
-                    </p>
-                </footer>
 
-            </div>
-        )
 
+    //Função de alerta para mostrar o resultado.
+    function evento(){
+        alert("Valores de A:" + a + ", de B: " + b + " e C: " + c)
     }
+
+
+    // ------------Front-end---------------
+    return (
+        <div className='Janela-Principal'>
+
+            <h1 className='Letra1'>Calculadora </h1>
+            <h1 className='Letra2'>de </h1>
+            <h1 className='Letra3'>pitagoras!</h1>
+
+
+
+            <div className='bloco'>
+                <p className="instrucao">
+                    Insira dois valores para calcular o lado que falta do
+                    triangulo:
+                </p>
+
+
+                <form class="form" id="form">
+                    <label for="a">
+                        a:
+                        <input name="a" id="a" type="number" value={a} onChange={(e)=> seta(e.target.value)}/>
+                    </label>
+                    <label for="b">
+                        b:
+                        <input name="b" id="b" type="number" value={b} onChange={(e)=> setb(e.target.value)} />
+                    </label>
+                    <label for="c">
+                        c:
+                        <input name="c" id="c" type="number" value={c} onChange={(e)=> setc(e.target.value)} />
+                    </label>
+                    <button onClick={evento}>Calcular</button>
+
+                </form>
+
+
+                <div className='imagem'>
+
+                    <img src="imagem/foto.png" alt='Imagem'></img>
+
+                </div></div>
+
+
+
+           
+
+        </div>
+    )
+
 }
+
+
+
+
 
 
 
